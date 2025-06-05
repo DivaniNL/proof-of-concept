30e212ae76dfe8dc244516449db0784681aa59c8
DivaniNL <dylanvannierop@gmail.com>
2025-06-05 21:47:39 +0200

    #2 added CSS enhancement [Frame with scrollsnap]

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ app.engine('liquid', engine.express())
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
-const artPiecesResponse = await fetch('https://openaccess-api.clevelandart.org/api/artworks/?limit=10');
+const artPiecesResponse = await fetch('https://www.rijksmuseum.nl/api/nl/collection?key=pWKXy0OF&ps=10');
 const artPiecesResponseJSON = await artPiecesResponse.json();
 
 app.get('/', async function (req, res) {
-  res.render('index.liquid', { artPieces: artPiecesResponseJSON.data })
+  res.render('index.liquid', { artPieces: artPiecesResponseJSON.artObjects })
 })
 const upload = multer({ dest: 'uploads/' });
 
