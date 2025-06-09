08058fd029095a54bd8f8baa5bd8014065924950
DivaniNL <dylanvannierop@gmail.com>
2025-06-09 12:59:41 +0200

    #8 added client side JS enhancement for pleasurable UI [TSDViewer]

--- public/js/detailViewer.js
+++ public/js/detailViewer.js
@@ -1,10 +1,20 @@
 
-
-let $aboutViewer = document.querySelector("#test2");
-TSDViewer.create($aboutViewer, {
-    model: 'hva-pictureframe3-horizontal',
-    plugins: 'customiser',
-    onLoadComplete: () => {
-        $aboutViewer.setTexture({name: 'base', source: document.querySelector('#test'), background: '#ff0000', scale: 'contain'}).then(() => { /* callback */ });
-    },
-});
+if (typeof TSDViewer !== 'undefined') {
+    if(TSDViewer){
+        document.querySelector('#model-alt img').setAttribute('style','visibility: hidden;');
+        let $aboutViewer = document.querySelector("#model-alt");
+        TSDViewer.create($aboutViewer, {
+            forceLoad: true,
+            model: 'hva-pictureframe3-horizontal',
+            plugins: 'customiser',
+            onLoadComplete: () => {
+                $aboutViewer.setTexture({name: 'field1', source: document.querySelector('#model-alt img'), background: '#ff0000', scale: 'cover'}).then(() => { /* callback */ });
+                $aboutViewer.setBackground("#2C2926");
+                document.querySelector("#model-alt img").remove();
+                $aboutViewer.classList.add('debug');
+            },
+        });
+    }
+}else{
+    console.log('TSD Viewer kan niet worden ingeladen, fukkie wukkie')
+}
